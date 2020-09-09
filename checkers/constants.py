33977a8d79265c5be032c3d1fad81fd976f67a39
zacikmareek <zacik.mareek@gmail.com>
2020-09-09 20:16:04 +0200

    Created Game Window

---
+++ checkers/constants.py
@@ -0,0 +1,11 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+#rgb colors for board 
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
